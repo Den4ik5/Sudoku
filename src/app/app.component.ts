f6a910afc406e3abe26ac744e715dfc5f65ab1d9
denis <polit.den63@gmail.com>
2021-06-25 16:49:37 +0300

    checkpoint commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public cdr: ChangeDetectorRef) {
+    this.generateDisplayableRows();
+  }
+
   title = 'sudoku';
+  timersTime = 0;
+  rows: any = [];
+
+  generateDisplayableRows() {
+    for (let i = 0; i < 9; i++) {
+      const row = [];
+      this.rows.push(row);
+      for (let j = 0; j < 9; j++) {
+        row.push( new Element(j.toString(), false));
+      }
+    }
+  }
+
+  startGame($event: MouseEvent) {
+    const source = timer(1000, 1000);
+    const subscribe = source.subscribe(val => {
+      console.log(val);
+      this.timersTime = +val * 1000;
+      this.cdr.detectChanges();
+    });
+
+  }
+
+  logger(rows: number[][]) {
+    console.log(this.rows);
+  }
+
+  logger2(elem: any) {
+    console.log(elem);
+  }
+}
+
+export class Element{
+  value: string;
+  shouldBeDisplayed: boolean;
+  constructor(value: string, shouldBeDisplayed: boolean) {
+    this.value = value;
+    this.shouldBeDisplayed = shouldBeDisplayed;
+  }
 }
